9362047bbb9b147e107fae91563bef1a0b3b3a44
João Felipe Lopes de Sus <joaofelipe1294@gmail.com>
2017-05-29 00:14:37 -0300

    Adicionada funcao de ativacao

--- perceptron.py
+++ perceptron.py
@@ -9,10 +9,16 @@ class Perceptron:
 	def aggregation_function(self, values):
 		return (values * self.weights).sum()
 
+	def activation_function(self, u):
+		if u >= 1:
+			return 1
+		else:
+			return 0
+
 	def fit(self, X, y):
 		pass
 		
 
 perceptron = Perceptron()
-f = perceptron.aggregation_function(np.array([3, 1, -1]))
-print(f)
+u = perceptron.aggregation_function(np.array([3, 1, -1]))
+print(perceptron.activation_function(u))
