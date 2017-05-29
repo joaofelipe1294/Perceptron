14619cbfddce38ab35d6518c408293b7e6f5c8ec
João Felipe Lopes de Sus <joaofelipe1294@gmail.com>
2017-05-29 00:02:55 -0300

    Adicionada geracao dos pesos iniciais da rede

--- perceptron.py
+++ perceptron.py
@@ -1,4 +1,13 @@
+import numpy as np
+
 class Perceptron:
 
 	def __init__(self):
+		self.bias = -1
+		self.weights = np.array(np.random.random_sample((3,)))
+
+	def fit(self, X, y):
 		pass
+		
+
+perceptron = Perceptron()
