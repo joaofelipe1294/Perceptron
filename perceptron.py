68516291c47812e1fb9afbe9061c543ff168b155
João Felipe Lopes de Sus <joaofelipe1294@gmail.com>
2017-05-29 00:13:22 -0300

    Adicionada funcao de agregacao

--- perceptron.py
+++ perceptron.py
@@ -5,9 +5,14 @@ class Perceptron:
 	def __init__(self):
 		self.bias = -1
 		self.weights = np.array(np.random.random_sample((3,)))
+		
+	def aggregation_function(self, values):
+		return (values * self.weights).sum()
 
 	def fit(self, X, y):
 		pass
 		
 
 perceptron = Perceptron()
+f = perceptron.aggregation_function(np.array([3, 1, -1]))
+print(f)
