81def319fafa65957461adff7ea36aac09d4c18e
João Felipe Lopes de Sus <joaofelipe1294@gmail.com>
2017-05-29 00:52:50 -0300

    Adicionado metodo que aplica algoritmo backpropagation

--- perceptron.py
+++ perceptron.py
@@ -21,16 +21,27 @@ class Perceptron:
 		prepared_values = np.concatenate((values, bias_array), axis = 1)
 		return prepared_values
 
+	def back_propagation(self, x_values, label_finded, label_correct):
+		error = label_correct - label_finded
+		self.weights = self.weights + error * x_values
+
 	def fit(self, X, y):
 		X_with_bias = self.insert_bias(X)
-		for line_index in range(0, X_with_bias.shape[0]):
-			values = X_with_bias[line_index, :]
-			u = self.aggregation_function(values)
-			output = self.activation_function(u)
-			#if output != y[line_index]:
-					
-			
+		while True:
+			error_count = 0
+			for line_index in range(0, X_with_bias.shape[0]):
+				x_values = X_with_bias[line_index, :]
+				u = self.aggregation_function(x_values)
+				output = self.activation_function(u)
+				if output != y[line_index]:
+					self.back_propagation(x_values, output, y[line_index])
+					error_count += 1
 
+			print(self.weights)
+			if input('Continue : ') != "s" or error_count == 0:
+				break
+
+	
 
 
 X = np.array([[1,1], [3, 1], [-1, 2], [2, -1], [-1, -1], [1, -3]])
@@ -38,6 +49,7 @@ y = [1,0,1,0,1,0]
 
 perceptron = Perceptron()
 perceptron.fit(X, y)
+
 #perceptron.insert_bias(np.array(([3, 1], [2, -1], [1, 1], [-1, -1])))
 #u = perceptron.aggregation_function(np.array([3, 1, -1]))
 #print(perceptron.activation_function(u))
